07640b88f2d551849ad52de8f4d00f1dc806968b
yosefurr <yosefanwar690@gmail.com>
2025-08-13 18:50:09 +0300

    added heart icon to favorite recipes

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,11 +1,11 @@
 import { bubblegum } from "@/utils/fonts";
 export default function Favorites() {
   return (
-    <div className=" flex flex-col justify-between gap-6 w-full max-w-[1500px] mt-30 mb-10 ">
-      <h2 className={` ${bubblegum.className} text-center text-5xl`}>
+    <div className="  w-full max-w-[1500px] pt-30 pb-10 min-h-[100vh] bg-white ">
+      <h2 className={` ${bubblegum.className} text-center text-5xl mb-15`}>
         Your Favorite Recipes
       </h2>
-      <section className="grid grid-cols-4 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-[1rem] max-w-[1000px] m-auto ">
+      <section className="flex flex-wrap justify-center m-auto gap-[1rem] max-w-[1000px] ">
         <div className="w-[200px] h-[200px] bg-red-400"></div>
         <div className="w-[200px] h-[200px] bg-red-400"></div>
         <div className="w-[200px] h-[200px] bg-red-400"></div>
--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,5 +1,7 @@
 "use client";
 import Link from "next/link";
+import { FaHeart } from "react-icons/fa";
+import { useState } from "react";
 export default function RecipeCard({
   id,
   title,
@@ -9,24 +11,35 @@ export default function RecipeCard({
   title: string;
   image: string;
 }) {
+  const [liked, setLiked] = useState(false);
   return (
-    <Link
-      href={`/${id}`}
-      key={id}
-      className=" group justify-self-center align-self-start border border-red-600 overflow-hidden pb-3 rounded-lg max-w-[300px] w-full bg-container flex flex-col justify-start items-center text-center gap-y-[20px] hover:bg-containerHover duration-300"
-    >
-      <div className=" overflow-hidden h-[180px] md:h-[200px] w-full min-w-[280px]">
-        <img
-          src={image}
-          alt="Meal Image"
-          onError={(e) => {
-            e.currentTarget.src = "/fallback.webp";
-          }}
-          loading="lazy"
-          className="w-full h-[180px] md:h-[200px] object-cover group-hover:scale-[1.1] group-hover:rotate-3 duration-300"
-        />
-      </div>
-      <p className=" max-w-[260px] font-bold">{title}</p>
-    </Link>
+    <div className="relative z-40  max-w-[300px] w-full">
+      <Link
+        href={`/${id}`}
+        key={id}
+        className=" group justify-self-center align-self-start border border-red-600 overflow-hidden pb-3 rounded-lg w-full h-full bg-container flex flex-col justify-start items-center text-center gap-y-[20px] hover:bg-containerHover duration-300"
+      >
+        <div className=" overflow-hidden h-[180px] md:h-[200px] w-full min-w-[280px]">
+          <img
+            src={image}
+            alt="Meal Image"
+            onError={(e) => {
+              e.currentTarget.src = "/fallback.webp";
+            }}
+            loading="lazy"
+            className="w-full h-[180px] md:h-[200px] object-cover group-hover:scale-[1.1] group-hover:rotate-3 duration-300"
+          />
+        </div>
+        <p className=" max-w-[260px] font-bold">{title}</p>
+      </Link>
+      <FaHeart
+        size={35}
+        className={`duration-150 p-2 bg-black/50 backdrop-filter-[15px] top-1 right-1 ${
+          liked ? "text-red-500" : "text-white"
+        } rounded-lg z-999 absolute `}
+        onClick={() => setLiked(!liked)}
+        style={{ cursor: "pointer" }}
+      />
+    </div>
   );
 }
